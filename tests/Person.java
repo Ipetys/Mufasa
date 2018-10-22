6a220b62f2127424eca0a82da29458e6724a5553
Ismo Lundbom <ismo.lundbom@gmail.com>
2018-10-22 19:10:49 +0300

    A version of mufasa. No mock, no testdoubles no nothing so far.

--- tests/Person.java
+++ tests/Person.java
@@ -9,4 +9,27 @@ public class Person {
 		fail("Not yet implemented");
 	}
 
+	
+	//Three tests for variables
+	//my 'Person' is 
+	@Test
+	public void UserDataTest() {
+		UserData us = new UserData("Seppo", "Niemi", "Sepi", "abcdefgh1234", "Finland", 
+			"20/20/1970", "05012345678", "dfgjdfkl@ggg.com");
+		assertEquals("Seppo","Seppo",us.getFirstName());
+	}
+	
+	@Test
+	public void UserDataTest_username() {
+		UserData us = new UserData("Seppo", "Niemi", "Sepi", "abcdefgh1234", "Finland", 
+			"20/20/1970", "05012345678", "dfgjdfkl@ggg.com");
+		assertEquals("Seppo","Sepi",us.getUserName());
+	}
+	
+	@Test
+	public void UserDataTest_gsm() {
+		UserData us = new UserData("Seppo", "Niemi", "Sepi", "abcdefgh1234", "Finland", 
+			"20/20/1970", "05012345678", "dfgjdfkl@ggg.com");
+		assertEquals("Seppo","05012345678",us.getGsm());
+	}	
 }
